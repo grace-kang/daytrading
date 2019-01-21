570a3c27b1995b1458276d2f31de2d4afb00e0e9
Jingjing Zhu <jzhu@pdftron.com>
2019-01-21 01:38:00 -0800

    format models

---
+++ models.go
@@ -0,0 +1,35 @@
+package main
+
+type TransType string
+
+type User struct {
+	username     string `json:"username"`
+	balance int `json:"balance"`
+}
+
+const (
+	BUY = TransType("BUY")
+	SELL = TransType("SELL")
+)
+
+type Transaction struct {
+	ID           int64     `json:"id"`
+	Username     string    `json:"username"`
+	StockSymbol  string    `json:"stock"`
+	Type         TransType `json:"type"`
+	Amount       int       `json:"amount"`
+	Cost int       `json:"cost"`
+	Time         int64     `json:"time"`
+}
+
+type Trigger struct {
+	ID           int64     `json:"id"`
+	Username     string    `json:"username"`
+	StockSymbol  string    `json:"stock"`
+	Order        TransType `json:"type"`
+	Amount       int       `json:"amount"`
+	TriggerPrice int       `json:"triggerprice"`
+	Executable   bool      `json:"executable"`
+	Time         int64     `json:"time"`
+}
+
