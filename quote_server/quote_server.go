c017be4bce6b323feb32794ed8ac02c50d03a01a
grace-kang <gracek316@gmail.com>
2019-02-27 12:17:33 -0800

    Use socket instead of http for quote server

--- quote_server/quote_server.go
+++ quote_server/quote_server.go
@@ -1,50 +1,80 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"net/http"
-	"time"
+  "fmt"
+  "math/rand"
+  "net"
+  "time"
+  "os"
+  "strings"
 )
 
 const (
-	connHost = "localhost" // Run on the local machine
-	connPort = "1200"      // Same port as on the regular system
-	connType = "tcp"       // NOTE: not HTPP
+  connHost = ""
+  connPort = "1200"
+  connType = "tcp"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":1200", nil)
+  // Listen for incoming connections.
+  l, err := net.Listen(connType, connHost+":"+connPort)
+  if err != nil {
+    fmt.Println("Error listening:", err.Error())
+    os.Exit(1)
+  }
+  // Close the listener when the application closes.
+  defer l.Close()
+  fmt.Println("Listening on " + connHost + ":" + connPort)
+  for {
+    // Listen for an incoming connection.
+    conn, err := l.Accept()
+    if err != nil {
+      fmt.Println("Error accepting: ", err.Error())
+      os.Exit(1)
+    }
+    // Handle connections in a new goroutine.
+    go handleRequest(conn)
+  }
 }
 
 // Handles incoming requests.
-func handler(w http.ResponseWriter, r *http.Request) {
-	params := r.URL.Query()
-	w.Write([]byte(makeResponse(params["stock"][0], params["user"][0])))
+func handleRequest(conn net.Conn) {
+  // Make a buffer to hold incoming data.
+  buf := make([]byte, 1024)
+  // Read the incoming connection into the buffer.
+  recv, err := conn.Read(buf)
+  if err != nil {
+    fmt.Println("Error reading:", err.Error())
+  }
+  split := strings.Split(string(buf[:recv]), ",")
+  time.Sleep(time.Duration(rand.Intn(30)) * time.Millisecond)
+  // Send a response back to person contacting us.
+  conn.Write([]byte(makeResponse(strings.TrimSpace(split[0]), strings.TrimSpace(split[1]))))
+  // Close the connection when you're done with it.
+  conn.Close()
 }
 
 func makeResponse(stock string, username string) string {
-	now := time.Now().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
-	amount := fmt.Sprintf("%d.%d", rand.Intn(500)+1, rand.Intn(100))
-	crypto := randSeq(25)
-	// (?P<quote>.+),(?P<stock>.+),(?P<user>.+),(?P<time>.+),(?P<key>.+)
-	output := fmt.Sprintf("%s,%s,%s,%d,%s\n",
-		amount,
-		stock,
-		username,
-		now,
-		crypto)
-	fmt.Println(output)
-	return output
+  now := time.Now().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
+  amount := fmt.Sprintf("%d.%d", rand.Intn(500)+1, rand.Intn(100))
+  crypto := randSeq(25)
+  // (?P<quote>.+),(?P<stock>.+),(?P<user>.+),(?P<time>.+),(?P<key>.+)
+  output := fmt.Sprintf("%s,%s,%s,%d,%s\n",
+  amount,
+  stock,
+  username,
+  now,
+  crypto)
+  fmt.Println(output)
+  return output
 }
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func randSeq(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
+  b := make([]rune, n)
+  for i := range b {
+    b[i] = letters[rand.Intn(len(letters))]
+  }
+  return string(b)
 }
