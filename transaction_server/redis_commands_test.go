748f1dcddcb89308f9a246e2ded92c76bb80d1d4
grace-kang <gracek316@gmail.com>
2019-03-15 16:27:31 -0700

    addBalance test

--- transaction_server/redis_commands_test.go
+++ transaction_server/redis_commands_test.go
@@ -6,6 +6,8 @@ import (
 
 func TestGetBalance(t *testing.T) {
 	client := dialRedis()
+	flushRedis(client)
+
 	username := "user"
 	balance := 1200.00
 
@@ -16,3 +18,16 @@ func TestGetBalance(t *testing.T) {
 	}
 }
 
+func TestAddBalance(t *testing.T) {
+	client := dialRedis()
+	flushRedis(client)
+
+	username := "user"
+	add := 300.00
+
+	addBalance(client, username, add)
+	result := getBalance(client, username)
+	if result != add {
+		t.Errorf("addBalance was incorrect, got: %f, want %f.", result, add)
+	}
+}
