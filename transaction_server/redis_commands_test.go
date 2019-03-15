7656adc7c8b7218106c63f2c9c3c102f7eedc6d9
grace-kang <gracek316@gmail.com>
2019-03-15 16:35:03 -0700

    stockOwned test

--- transaction_server/redis_commands_test.go
+++ transaction_server/redis_commands_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
+/*
+/ These tests assume that the functions
+/ dialRedis() and flushRedis() are
+/ working properly. If they are not,
+/ no tests in this test suite will
+/ pass.
+*/
+
 func TestGetBalance(t *testing.T) {
 	client := dialRedis()
 	flushRedis(client)
@@ -29,5 +37,26 @@ func TestAddBalance(t *testing.T) {
 	result := getBalance(client, username)
 	if result != add {
 		t.Errorf("addBalance was incorrect, got: %f, want %f.", result, add)
+	}
+}
+
+func TestStockOwned(t *testing.T) {
+	client := dialRedis()
+	flushRedis(client)
+
+	username := "user"
+	stock := "ABC"
+
+	result := stockOwned(client, username, stock)
+	if result != 0 {
+		t.Errorf("stockOwned was incorrect, got %d, want %d.", result, 0)
+	}
+
+	amount := 31
+
+	client.Cmd("HSET", username, stock, amount)
+	result2 := stockOwned(client, username, stock)
+	if result2 != amount {
+		t.Errorf("stockOwned was incorrect, got %d, want %d.", result, amount)
 	}
 }
