4c034a549c4dda1bb06d30eeca3df0ad1d79b440
Jingjing Zhu <jzhu@pdftron.com>
2019-03-23 14:08:54 -0700

    use a function to pass param into page

--- Servers/web_server/web_server.go
+++ Servers/web_server/web_server.go
@@ -58,21 +58,18 @@ func outputHTML(w http.ResponseWriter, filename string, data interface{}) {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// p := "test"
-	// t := template.Must(template.ParseFiles("tmp/home.html"))
-	// t.Execute(w, p)
-
-	// quotey := getQuote("abc", "123")
-	// fmt.Println(string(quotey))
-	// myvar := map[string]interface{}{"Quote": quotey}
-	// outputHTML(w, "tmp/home.html", myvar)
 
 	quotey := getQuote("abc", "123")
 	fmt.Println(string(quotey))
-	tpl, _ := ioutil.ReadFile("tmp/home.html")
-	tplParsed, _ := template.New("test").Parse(string(tpl))
-	templateData := map[string]interface{}{"Quote": quotey}
-	tplParsed.Execute(w, templateData)
+	myvar := map[string]interface{}{"Quote": quotey}
+	outputHTML(w, "tmp/home.html", myvar)
+
+	// quotey := getQuote("abc", "123")
+	// fmt.Println(string(quotey))
+	// tpl, _ := ioutil.ReadFile("tmp/home.html")
+	// tplParsed, _ := template.New("test").Parse(string(tpl))
+	// templateData := map[string]interface{}{"Quote": quotey}
+	// tplParsed.Execute(w, templateData)
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
